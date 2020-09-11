d424c3d0af1ed5569fb7fb163379f8b7cb02142a
Robson Inocêncio <robson.inocencio@gmail.com>
2020-09-11 18:08:34 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - School of net
         </a>
       </header>
     </div>
